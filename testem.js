69d62053ec573fa0e584854994ca26943e3c7e97
Michael Carroll <juwara0@users.noreply.github.com>
2017-03-07 14:45:55 -0600

    update to use lint from

--- testem.js
+++ testem.js
@@ -1,16 +1,16 @@
-/* jshint node:true*/
 var Reporter = require('ember-test-utils/reporter')
 
 module.exports = {
-  'framework': 'mocha',
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
+  disable_watching: true,
+  framework: 'mocha',
+  launch_in_ci: [
+    'Chrome',
     'Firefox'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'Chrome'
   ],
   parallel: '-1',
-  reporter: new Reporter()
+  reporter: new Reporter(),
+  test_page: 'tests/index.html?hidepassed'
 }
